dfff8016f30e7e7ee99445b40aac711dd92a698a
Abdullah Al Hisham <ahisham@medavante.com>
2017-09-15 15:11:57 +0600

    Fixed locators

--- src/main/java/net/phptravels/ui/pages/Header.java
+++ src/main/java/net/phptravels/ui/pages/Header.java
@@ -9,13 +9,16 @@ public class Header {
 	@FindBy(how = How.CSS, using = "img.logo")
 	private WebElement logo;
 	
-	@FindBy(how = How.CSS, using = "li.pull-right a.dropdown-toggle")
+//	@FindBy(how = How.CSS, using = "li.pull-right a.dropdown-toggle")
+	@FindBy(how = How.CSS, using = "ul.navbar-side:nth-child(1) > li:nth-child(1)")
 	private WebElement userControlMenu;
 
-	@FindBy(how = How.XPATH, using = "//a[contains(text(), 'Account')]")
+//	@FindBy(how = How.XPATH, using = "//a[contains(text(), 'Account')]")
+	@FindBy(how = How.CSS, using = "li.open ul.dropdown-menu > li:nth-child(1)")
 	private WebElement accountOption;
 	
-	@FindBy(how = How.XPATH, using = "//a[contains(text(), 'Logout')]")
+//	@FindBy(how = How.XPATH, using = "//a[contains(text(), 'Logout')]")
+	@FindBy(how = How.CSS, using = "li.open ul.dropdown-menu > li:nth-child(2)")
 	private WebElement logoutOption;
 	
 	public WebElement getLogo() {
